2a52f766c097286e2ed6aa02e9b9b256e897ff35
Florian Schwabe <florian.schwabe@hotmail.de>
2019-10-20 01:55:48 +0200

    Simplify reading packets and some variable renaming

--- TicTacToeCore/Core/SocketUtil.cs
+++ TicTacToeCore/Core/SocketUtil.cs
@@ -5,63 +5,45 @@ using TicTacToe.Core.Packet;
 
 namespace TicTacToe.Core {
     public static class SocketUtil {
-            public static void SendPacket(this Socket socket, IPacket packet) {
-                MemoryStream packetData = new MemoryStream();
-                PacketDataWriter packetDataWriter = new PacketDataWriter(new BinaryWriter(packetData));
-                
-                int packetDataLength = packetDataWriter.Write(packet);
-                int totalLength = 2 * sizeof(int) + packetDataLength; // Packet id + Packet data length + Packet data
-                
-                packetDataWriter.Flush();
-                byte[] data = packetData.GetBuffer();
-                packetData.Close();
-                
-                byte[] packetBuffer = new byte[totalLength];
-                packetData = new MemoryStream(packetBuffer);
-                BinaryWriter packetWriter = new BinaryWriter(packetData);
-                
-                packetWriter.Write(PacketRegistry.GetId(packet));
-                packetWriter.Write(packetDataLength);
-                packetWriter.Write(data, 0, packetDataLength);
+        public static void SendPacket(this Socket socket, IPacket packet) {
+            MemoryStream packetDataStream = new MemoryStream();
+            PacketDataWriter packetDataWriter = new PacketDataWriter(new BinaryWriter(packetDataStream));
 
-                socket.Send(packetBuffer);
-            }
+            int packetDataLength = packetDataWriter.Write(packet);
+            int totalPacketLength = 2 * sizeof(int) + packetDataLength; // Packet id + Packet data length + Packet data
 
-            public static IPacket ReceivePacket(this Socket socket, Direction direction) {
-                BinaryReader reader = socket.Reader();
+            packetDataWriter.Flush();
+            byte[] packetData = packetDataStream.GetBuffer();
+            packetDataStream.Close();
 
-                int packetId = reader.ReadInt32();
-                
-                IPacket packet = PacketRegistry.FromId(packetId, direction);
-                
-                int dataLength = reader.ReadInt32();
+            byte[] fullPacketBuffer = new byte[totalPacketLength];
+            MemoryStream fullPacketData = new MemoryStream(fullPacketBuffer);
+            BinaryWriter packetWriter = new BinaryWriter(fullPacketData);
 
-                byte[] dataBuffer = reader.ReadBytes(dataLength);
-                
-                MemoryStream memoryStream = new MemoryStream(dataBuffer, false);
-                packet.Read(new PacketDataReader(new BinaryReader(memoryStream)));
+            packetWriter.Write(PacketRegistry.GetId(packet));
+            packetWriter.Write(packetDataLength);
+            packetWriter.Write(packetData, 0, packetDataLength);
 
-                return packet;
-            }
-
-            public static BinaryReader Reader(this Socket socket, int bufferSize = 256) {
-                if (bufferSize <= 0) {
-                    throw new ArgumentException($"{nameof(bufferSize)} must be > 0");
-                }
-
-                MemoryStream memoryStream = new MemoryStream();
-
-                byte[] buffer = new byte[bufferSize];
-                int read;
-
-                do {
-                    read = socket.Receive(buffer);
-                    memoryStream.Write(buffer, 0, read);
-                } while (read > 256);
-
-                memoryStream.Position = 0;
-
-                return new BinaryReader(memoryStream);
-            }
+            socket.Send(fullPacketBuffer);
         }
+
+        public static IPacket ReceivePacket(this Socket socket, Direction direction) {
+            byte[] intBuffer = new byte[4];
+            socket.Receive(intBuffer);
+            int packetId = BitConverter.ToInt32(intBuffer);
+
+            IPacket packet = PacketRegistry.FromId(packetId, direction);
+
+            socket.Receive(intBuffer);
+            int dataLength = BitConverter.ToInt32(intBuffer);
+
+            byte[] packetDataBuffer = new byte[dataLength];
+            socket.Receive(packetDataBuffer);
+
+            MemoryStream memoryStream = new MemoryStream(packetDataBuffer, false);
+            packet.Read(new PacketDataReader(new BinaryReader(memoryStream)));
+
+            return packet;
+        }
+    }
 }
